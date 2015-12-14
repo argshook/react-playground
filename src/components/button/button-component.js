8202f90a23c33dd0446e1be735e715258440a477
argshook <argshook@gmail.com>
2015-12-15 01:33:46 +0200

    button component

--- src/components/button/button-component.js
+++ src/components/button/button-component.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
-let count = 0;
-
 export default class Button extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      count: 0
+    };
+  }
+
+  onClick(e) {
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
+
   render() {
     return (
-      <button onClick={() => {count += 1;}}>{count}</button>
+      <button onClick={this.onClick.bind(this)}>{this.state.count}</button>
     );
   }
 };
